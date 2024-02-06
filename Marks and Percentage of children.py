3107a1bf0d58498fea94a0781639b31fef6b270a
Ra1kumar <ravikumarchowdary@gmail.com>
2024-02-06 14:41:10 +0530

    added a python file

---
+++ Marks and Percentage of children.py
@@ -0,0 +1,11 @@
+#Find the total percentage and marks of the Student
+rno=int(input("Enter the Roll Number:"))
+name=input("Enter the Student Name:")
+sub1=int(input("Enter the Marks of Telugu:"))
+sub2=int(input("Enter the marks of Hindi:"))
+sub3=int(input("Enter the marks of the English:"))
+Total=sub1+sub2+sub3
+percentage=Total/3/100
+print("Total:",Total)
+print("percentage:",percentage)
+print("Roll no:",rno,"Student name:",name,"Telugu:",sub1,"Hindi",sub2,"English",sub3)
